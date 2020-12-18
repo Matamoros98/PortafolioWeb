f781e0d99111f841e2820f8255fc96c20cd9271f
Christian Matamoros <christianmatamoros98@gmail.com>
2020-12-17 21:24:31 -0500

    diseño

---
+++ js/escritura.js
@@ -0,0 +1,30 @@
+
+let wr = (str) => {
+  let arrFromSrt = str.split('');
+  let i = 0;
+  let printStr = setInterval(function () {
+    document.getElementById("mensaje").innerHTML += arrFromSrt[i];
+    i++;
+
+    if(i === arrFromSrt.length)
+    {
+        clearInterval(printStr);
+        wrp('soy Desarrollador Web');
+    }
+  }, 300);
+};
+let wrp = (str) => {
+    let arrFromSrt = str.split('');
+    let i = 0;
+    let printStr = setInterval(function () {
+      document.getElementById("sub-mensaje").innerHTML += arrFromSrt[i];
+      i++;
+  
+      if(i === arrFromSrt.length)
+      {
+          clearInterval(printStr);
+      }
+    }, 300);
+  };
+wr('Hola,soy Christian Matamoros');
+
