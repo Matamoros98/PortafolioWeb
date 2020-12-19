b6bd0113ae8c1e7347a95bac4fc2e96716fc1f45
Christian Matamoros <christianmatamoros98@gmail.com>
2020-12-18 22:50:23 -0500

    avance

---
+++ js/es2.js
@@ -0,0 +1,22 @@
+let wr = (str, str2) => {
+  let arrFromSrt = str.split("");
+  let i = 0;
+  let printStr = setInterval(function () {
+    document.getElementById("mensaje").innerHTML += arrFromSrt[i];
+    i++;
+    if (i === arrFromSrt.length) {
+      clearInterval(printStr);
+      i = 0;
+      arrFromSrt.length = 0;
+      arrFromSrt = str2.split("");
+      let printStr2 = setInterval(function () {
+        document.getElementById("sub-mensaje").innerHTML += arrFromSrt[i];
+        i++;
+        if (i === arrFromSrt.length) {
+          clearInterval(printStr2);
+        }
+      }, 300);
+    }
+  }, 300);
+};
+wr("Hola,soy Christian Matamoros", "Soy Desarrollador Web");
---
+++ js/es3.js
@@ -0,0 +1,8 @@
+let wr = (str, str2) => {
+    let arrFromSrt = str.split("");
+    let arrFromSrt2 = str.split("");
+    let i = 0;
+    
+
+  };
+  wr("Hola,soy Christian Matamoros", "Soy Desarrollador Web");
